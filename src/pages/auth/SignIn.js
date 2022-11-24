6da0309c7d310be9902f3772e2ca0536d17d6197
Mohamed <nadaaramn@gmail.com>
2022-11-24 13:44:26 +0300

    auth service

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,12 +1,24 @@
 // import node module libraries
-import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { Col, Row, Card, Form, Button, Image } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { login } from "services/authService";
+
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Card, Form, Button, Image } from "react-bootstrap";
 
 // import media files
-import Logo from 'assets/images/brand/logo/logo-icon.svg';
+import Logo from "assets/images/brand/logo/logo-icon.svg";
+
+const getData = async () => {
+  await login({
+    username: "252615129181",
+    password: "8085",
+  });
+};
+
 export default function SignIn() {
-  return (
-    <div>SignIn</div>
-  )
+  useEffect(() => {
+    getData();
+  }, []);
+  return <div>SignIn</div>;
 }
