25c325a7fff8193a092bfac9f89d625bbca88479
Mohamed <nadaaramn@gmail.com>
2022-12-02 14:58:17 +0300

    i've nothing to update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 // import node module libraries
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 // import layouts
 
-import AllRoutes from 'layouts/AllRoutes';
-import ScrollToTop from 'layouts/ScrollToTop';
+import AllRoutes from "layouts/AllRoutes";
+import ScrollToTop from "layouts/ScrollToTop";
 // import required stylesheet
-import 'simplebar/dist/simplebar.min.css';
-import 'tippy.js/animations/scale.css';
-import { CurrentUserContext } from 'services/currentUserContext';
-import { useState,useEffect } from 'react';
-import { getLoggedInUser } from 'services/authService';
+import "simplebar/dist/simplebar.min.css";
+import "tippy.js/animations/scale.css";
+import { CurrentUserContext } from "services/currentUserContext";
+import { useState, useEffect } from "react";
+import { getLoggedInUser } from "services/authService";
 function App() {
-	const [theUser, setTheUser] = useState(getLoggedInUser());
-	// const getCurrentUser = async () => {
-	// 	const respData = await getLoggedInUser();
-	// 	console.log(respData);
-	// 	setTheUser(respData);
-	// };
-	// useEffect(() => {
-	// 	getCurrentUser();
-	// }, null);
-	return (
-		<Router>
-			<CurrentUserContext.Provider value={{ theUser, setTheUser }} >
+  const [user, setUser] = useState([]);
 
+  const getCurrentUser = async () => {
+    const { data } = await getLoggedInUser();
+    console.log(data);
+    setUser(data);
+  };
 
-				<div className="App">
-					<ScrollToTop />
-					<AllRoutes />
-				</div>
-			</CurrentUserContext.Provider>
-		</Router>
-	);
+  console.log(user);
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  //   console.log(user);
+  return (
+    <Router>
+      <CurrentUserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <ScrollToTop />
+          <AllRoutes />
+        </div>
+      </CurrentUserContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
